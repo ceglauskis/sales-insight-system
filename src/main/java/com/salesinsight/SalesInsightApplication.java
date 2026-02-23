79dd2d553a269c2ad3e295a8cb3788fd689a0bea
maikoceglauskis@gmail.com <maikoceglauskis@gmail.com>
2026-02-23 17:05:04 -0300

    feat: project setup - structure, docker-compose and application.yml

---
+++ src/main/java/com/salesinsight/SalesInsightApplication.java
@@ -0,0 +1,13 @@
+package com.salesinsight;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SalesInsightApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SalesInsightApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/salesinsight/SalesInsightApplicationTests.java
@@ -0,0 +1,13 @@
+package com.salesinsight;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SalesInsightApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
